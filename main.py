c9473f97ce105175eeb2137228609d4a590e685a
Edward <hua9939@gmail.com>
2023-12-11 22:12:00 +1300

    modify the page setting

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
-    for i in range(row["Pages"]):
+    pdf.add_page()
+
+    pdf.set_font(family="arial", style="B", size=24)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=0, h=12, txt=row["Topic"], border=0, align="L", ln=1)
+    pdf.line(10, 22, 200, 22)
+    for i in range(row["Pages"]-1):
         pdf.add_page()
 
-        pdf.set_font(family="arial", style="B", size=24)
-        pdf.set_text_color(0, 0, 0)
-        pdf.cell(w=0, h=12, txt=row["Topic"], border=0, align="L", ln=1)
-        pdf.line(10, 22, 200, 22)
 
 
 
